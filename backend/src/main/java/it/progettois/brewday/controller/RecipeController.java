b6c1a0079e313ac4a049b169f0b9c9622bda7a96
mazzupa <p.mazzucchetti@campus.unimib.it>
2021-01-02 19:30:32 +0100

    Repository and model POC

--- backend/src/main/java/it/progettois/brewday/controller/RecipeController.java
+++ backend/src/main/java/it/progettois/brewday/controller/RecipeController.java
@@ -1,18 +1,34 @@
 package it.progettois.brewday.controller;
 
+import it.progettois.brewday.model.Recipe;
 import it.progettois.brewday.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RecipeController {
 
+    private final RecipeService recipeService;
+
     @Autowired
-    public RecipeService recipeService;
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @GetMapping("/recipe")
-    public String getRecipes(){
-        return recipeService.getRecipes();
+    public ResponseEntity<?> getRecipes() {
+
+        List<Recipe> recipes = this.recipeService.getRecipes();
+
+        if (recipes.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No recipes found");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(recipes);
+        }
     }
 }
---
+++ backend/src/main/java/it/progettois/brewday/model/Recipe.java
@@ -0,0 +1,20 @@
+package it.progettois.brewday.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String recipeName;
+}
