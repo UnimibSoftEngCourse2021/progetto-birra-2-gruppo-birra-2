569d8a543efefac2876fdc8f8371e3b4cccad07a
Patrick Mazzucchetti <patrick.mazzu@gmail.com>
2021-01-13 15:27:21 +0100

    Updated travis for python microservices

---
+++ script/deploy.sh
@@ -0,0 +1,11 @@
+#bin/bash
+
+docker stop brewday
+docker rm brewday
+docker image rm gruppobirra2/brewday
+docker run -p 8080:8080 -d --name brewday gruppobirra2/brewday:latest
+
+docker stop maximizebrewtoday
+docker rm maximizebrewtoday
+docker image rm gruppobirra2/maximizebrewtoday
+docker run -p 5000:5000 -d --name maximizebrewtoday gruppobirra2/maximizebrewtoday:latest
