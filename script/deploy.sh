7ad82a5d38231b955b50d2598cc09be24a9f7730
Patrick Mazzucchetti <patrick.mazzu@gmail.com>
2021-01-13 16:24:03 +0100

    Dokerized frontend

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,3 +9,8 @@ docker stop maximizebrewtoday
 docker rm maximizebrewtoday
 docker image rm gruppobirra2/maximizebrewtoday
 docker run -p 5000:5000 -d --name maximizebrewtoday gruppobirra2/maximizebrewtoday:latest
+
+docker stop brewday-frontend
+docker rm brewday-frontend
+docker image rm gruppobirra2/brewday-frontend
+docker run -p 8080:8080 -d --name brewday-frontend gruppobirra2/brewday-frontend:latest
