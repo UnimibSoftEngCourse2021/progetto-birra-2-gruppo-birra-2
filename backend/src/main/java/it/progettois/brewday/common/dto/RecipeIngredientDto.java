571b08ab2ceeb2e97a3aab27fdd4802723fd0da5
Ricky <ricc98@gmail.com>
2021-01-24 14:47:36 +0100

    Recipe tests completed

--- backend/src/main/java/it/progettois/brewday/common/dto/RecipeIngredientDto.java
+++ backend/src/main/java/it/progettois/brewday/common/dto/RecipeIngredientDto.java
@@ -1,12 +1,24 @@
 package it.progettois.brewday.common.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class RecipeIngredientDto {
+
+    public RecipeIngredientDto() {
+    }
+
+    public RecipeIngredientDto(Integer recipeIngredientId, Integer ingredientId, String ingredientName, Double quantity) {
+        this.recipeIngredientId = recipeIngredientId;
+        this.ingredientId = ingredientId;
+        this.ingredientName = ingredientName;
+        this.quantity = quantity;
+    }
 
     private Integer recipeIngredientId;
     private Integer ingredientId;
     private String ingredientName;
-    private Integer quantity;
+    private Double quantity;
 }
