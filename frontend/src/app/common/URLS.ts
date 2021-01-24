ca9c825c6f6e385d596fc0f60c13ec2d6dcb1428
Ricky <ricc98@gmail.com>
2021-01-25 00:20:20 +0100

    Fixed Brew (?)

--- frontend/src/app/common/URLS.ts
+++ frontend/src/app/common/URLS.ts
@@ -42,7 +42,7 @@ export enum Actions {
 export const BASE_URL_DEV = 'http://ec2-18-156-174-69.eu-central-1.compute.amazonaws.com:8080/';
 export const BASE_URL_LOCALHOST = 'http://localhost:8080/';
 
-export const BASE_URL = BASE_URL_DEV;
+export const BASE_URL = BASE_URL_LOCALHOST;
 
 export const URLS: Url[] = [
 
