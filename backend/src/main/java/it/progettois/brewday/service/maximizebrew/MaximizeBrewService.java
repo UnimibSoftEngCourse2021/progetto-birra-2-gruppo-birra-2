17fbbe38a6edf998fafc059e4c4970fac4c5bb3c
Patrick Mazzucchetti <patrick.mazzu@gmail.com>
2021-01-25 18:13:38 +0100

    Update url

--- backend/src/main/java/it/progettois/brewday/service/maximizebrew/MaximizeBrewService.java
+++ backend/src/main/java/it/progettois/brewday/service/maximizebrew/MaximizeBrewService.java
@@ -3,7 +3,7 @@ package it.progettois.brewday.service.maximizebrew;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "MaximizeBrewService", url = "http://ec2-18-156-174-69.eu-central-1.compute.amazonaws.com:5000")
+@FeignClient(name = "MaximizeBrewService", url = "${MaximizeBrewService.ribbon.listOfServers}")
 public interface MaximizeBrewService {
 
     @PostMapping()
