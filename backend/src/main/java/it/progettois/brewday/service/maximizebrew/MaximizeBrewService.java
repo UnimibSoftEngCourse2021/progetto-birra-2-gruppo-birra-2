913fa7e5a7b61201219acf790550db4a80540993
Ricky <ricc98@gmail.com>
2021-01-25 16:32:56 +0100

    maximizebrewservice url updated

--- backend/src/main/java/it/progettois/brewday/service/maximizebrew/MaximizeBrewService.java
+++ backend/src/main/java/it/progettois/brewday/service/maximizebrew/MaximizeBrewService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 //aws url: "http://ec2-18-156-174-69.eu-central-1.compute.amazonaws.com:5000"
 
-@FeignClient(name = "MaximizeBrewService", url = "http://0.0.0.0:5000/")
+@FeignClient(name = "MaximizeBrewService", url = "http://ec2-18-156-174-69.eu-central-1.compute.amazonaws.com:5000")
 public interface MaximizeBrewService {
 
     @PostMapping()
