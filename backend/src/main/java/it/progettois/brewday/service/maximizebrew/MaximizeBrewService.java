2785310179feaea2e5de7caf356cb606c9068dbf
Patrick Mazzucchetti <patrick.mazzu@gmail.com>
2021-01-26 09:40:03 +0100

    Completed prod infrastructure

--- backend/src/main/java/it/progettois/brewday/service/maximizebrew/MaximizeBrewService.java
+++ backend/src/main/java/it/progettois/brewday/service/maximizebrew/MaximizeBrewService.java
@@ -3,7 +3,7 @@ package it.progettois.brewday.service.maximizebrew;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "MaximizeBrewService", url = "${MaximizeBrewService.ribbon.listOfServers}")
+@FeignClient(name = "MaximizeBrewService", url = "${feign.client.url.serviceUrl}")
 public interface MaximizeBrewService {
 
     @PostMapping()
